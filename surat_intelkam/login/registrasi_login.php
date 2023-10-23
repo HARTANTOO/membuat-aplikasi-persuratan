30493e71a7542a744e5b28807a1782af69311c0b
HARTANTOO <86630615+HARTANTOO@users.noreply.github.com>
2023-10-23 12:26:50 +0700

    menambahkan proses registrasi

--- surat_intelkam/login/registrasi_login.php
+++ surat_intelkam/login/registrasi_login.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+include "../surat_biasa/koneksi.php";
+
+if (isset($_POST['registrasi'])) {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $simpan = mysqli_query(
+        mysqli_connect($server, $username, $password, $database),
+        "INSERT INTO admin (username, password) VALUE ('$username', '$password')"
+    );
+
+    //konfirmasi berhasil apa tidak
+    if ($simpan) {
+        echo "registrasi berhasil";
+        header('location: login.php');
+        exit;
+    } else {
+        echo "registrasi gagal";
+        exit;
+    }
+}
+
+//jika tombol di tekan
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,14 +38,14 @@
 
 <body>
     <h2 align="center">Form Register</h2>
-    <form action="proses_registrasi_form.php" method="POST" align="center">
+    <form action="" method="POST" align="center">
         <label for="username">Nama Lengkap :</label>
         <input type="text" name="username" id="username" placeholder="username" autocomplete="off"><br>
 
         <label for="password">Password :</label>
         <input type="password" name="password" id="password" placeholder="password" autocomplete="off"><br>
 
-        <input type="submit" name="simpan" value="simpan">
+        <input type="submit" name="registrasi" value="simpan">
     </form>
 </body>
 
